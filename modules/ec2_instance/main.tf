ab65fe301dd95899cd5f3adc8ff957a94ad307c1
diochang <dio432.chang@gmail.com>
2021-05-26 17:54:58 +0800

    end

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -6,7 +6,7 @@ data "aws_ssm_parameter" "linux_ami" {
     name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
-resource "aws_ssm_parameter" "dio_ami" {
+resource "aws_ssm_parameter" "linux_ami" {
     ami               = data.aws_ssm_parameter.linux_ami.value
     instance_type     = var.instance_type
     availability_zone = var.instance_az
--- modules/ec2_instance/output.tf
+++ modules/ec2_instance/output.tf
@@ -1,5 +1,5 @@
 output "private_IP" {
     description = "aws_ssm_parameter_linux_ami_IP"
-    value = "aws_ssm_parameter_dio_ami.private_ip"
+    value = "aws_ssm_parameter_linux_ami.private_ip"
 
 }
